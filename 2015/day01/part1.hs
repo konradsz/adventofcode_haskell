ad32c25136b6b0bfdd4ce782cda3cbcf5e054603
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2022-07-14 22:46:49 +0200

    add solutions for day01 2015

---
+++ 2015/day01/part1.hs
@@ -0,0 +1,12 @@
+main = do
+  interact (show . calculateFloor)
+
+calculateFloor :: String -> Int
+calculateFloor =
+  foldl
+    ( \acc x -> case x of
+        '(' -> acc + 1
+        ')' -> acc - 1
+        _ -> error "unrecognizable input"
+    )
+    0
---
+++ 2015/day01/part2.hs
@@ -0,0 +1,15 @@
+main = do
+  interact (show . findBasement)
+
+findBasement :: String -> Int
+findBasement =
+  length
+    . takeWhile
+      (/= -1)
+    . scanl
+      ( \acc x -> case x of
+          '(' -> acc + 1
+          ')' -> acc - 1
+          _ -> error "unrecognizable input"
+      )
+      0
