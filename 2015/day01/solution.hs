a13c88d4c956a12a6aa22f9875470ab63d7a2500
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2022-07-16 15:12:07 +0200

    move solution to a single file

---
+++ 2015/day01/solution.hs
@@ -0,0 +1,21 @@
+import System.Directory.Internal.Prelude (getArgs)
+
+main :: IO ()
+main = do
+  (input : _) <- getArgs
+  input <- readFile input
+  print $ part1 input
+  print $ part2 input
+
+part1 :: String -> Int
+part1 = foldl takeStep 0
+
+part2 :: String -> Int
+part2 = length . takeWhile (/= -1) . scanl takeStep 0
+
+takeStep :: Int -> Char -> Int
+takeStep floor dir =
+  case dir of
+    '(' -> floor + 1
+    ')' -> floor - 1
+    _ -> error "unrecognizable input"
