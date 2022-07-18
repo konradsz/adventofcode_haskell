17edf5a0296c64ca2997533e86ab764b235d99c3
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2022-07-18 22:05:37 +0200

    add solution to part1 of day03 2015

---
+++ 2015/day03/solution.hs
@@ -0,0 +1,27 @@
+import qualified Data.Set as Set
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  (input : _) <- getArgs
+  input <- readFile input
+  print $ part1 input
+
+part1 :: String -> Int
+part1 input =
+  length . fst $
+    foldl
+      ( \(visited, (x, y)) dir ->
+          let newPos = move dir (x, y)
+           in (Set.insert newPos visited, newPos)
+      )
+      (Set.singleton (0, 0), (0, 0))
+      input
+
+move :: Char -> (Int, Int) -> (Int, Int)
+move dir (x, y) = case dir of
+  '^' -> (x, y + 1)
+  'v' -> (x, y - 1)
+  '>' -> (x + 1, y)
+  '<' -> (x - 1, y)
+  _ -> error "incorrect input"
