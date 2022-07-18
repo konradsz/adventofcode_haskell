3651524af5704fb6436c4bf105d63fd370a9ea0d
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2022-07-18 22:56:21 +0200

    add solution for part2 day03 2015

--- 2015/day03/solution.hs
+++ 2015/day03/solution.hs
@@ -6,10 +6,21 @@ main = do
   (input : _) <- getArgs
   input <- readFile input
   print $ part1 input
+  print $ part2 input
 
 part1 :: String -> Int
-part1 =
-  length . fst
+part1 = length . visit
+
+part2 :: String -> Int
+part2 input =
+  length $
+    Set.union
+      (visit $ every2nd input)
+      (visit $ every2nd $ tail input)
+
+visit :: String -> Set.Set (Int, Int)
+visit =
+  fst
     . foldl
       ( \(visited, (x, y)) dir ->
           let newPos = move dir (x, y)
@@ -24,3 +35,8 @@ move dir (x, y) = case dir of
   '>' -> (x + 1, y)
   '<' -> (x - 1, y)
   _ -> error "incorrect input"
+
+every2nd :: [a] -> [a]
+every2nd [] = []
+every2nd [x] = [x]
+every2nd (x : xs) = x : every2nd (tail xs)
